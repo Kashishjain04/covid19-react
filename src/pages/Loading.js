f312b6717268542f5583f6f1d1a8f4c7619711d1
Kashish Jain <jainabhishek7204@gmail.com>
2021-06-21 00:26:50 +0530

    added pulse loader animation

--- src/pages/Loading.js
+++ src/pages/Loading.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loading = (
   <div className="grid place-items-center h-screen">
-    <img src="/logo.png" alt="logo" className="max-w-xs" />
+    <img src="/logo.png" alt="logo" className="max-w-xs animate-pulse" />
   </div>
 );
 
